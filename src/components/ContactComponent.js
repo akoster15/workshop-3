f4bb777f7ae9216dd49e89085e296b81930721cb
philip howley <howley.phil@gmail.com>
2022-01-29 12:42:07 -0500

    Fixed issues with missing code

---
+++ src/components/ContactComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ContactComponent() {
+  return <div>contact</div>;
+}
+
+export default ContactComponent;
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HeaderComponent() {
+  return <div>HC</div>;
+}
+
+export default HeaderComponent;
